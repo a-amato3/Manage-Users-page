57de28026db8d1556329d43dc220fd589d9aa19e
Aaron Amato <amato@pnptc.com>
2020-09-21 16:03:28 +0200

    dist folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserFormComponent } from './user-form/user-form.component';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireFunctionsModule } from '@angular/fire/functions';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddUserComponent } from './add-user/add-user.component';
-import { AngularFireAuthModule } from '@angular/fire/auth';
 
 
 @NgModule({
@@ -29,7 +28,6 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule,
     AngularFireFunctionsModule
   ],
   providers: [],
